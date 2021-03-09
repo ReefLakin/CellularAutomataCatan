50c25aa2b8de7f12ecd2938aaa9af0085419433d
ReefLakin <reeflakin2@gmail.com>
2021-03-09 18:26:52 +0000

    Hex Grid Added
    Stole grid for the hexagon Catan board from "https://codepen.io/MattH22/pen/pqFLJ" and also added some Bootstrap scripts.

--- index.php
+++ index.php
@@ -1,5 +1,93 @@
 <?php
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!-- Required Meta -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<h1 style="text-align: center">Cellular Automata Catan</h1>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!-- Link to Main Stylesheet -->
+    <link rel="stylesheet" type="text/css" href="styles/styles.css">
+
+    <!-- Title -->
+    <title>Cellular Automata Catan</title>
+</head>
+<body>
+
+<!-- Stolen from the legend that is MattH22: https://codepen.io/MattH22/pen/pqFLJ -->
+
+<div id="hexe">
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div><br />
+    <div class="even">
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+    </div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="even">
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+    </div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="even">
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+    </div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="even">
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+    </div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="even">
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+        <div class="hex hex-row"></div>
+    </div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="hex hex-row"></div>
+    <div class="even">
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+        <div class="hex"></div>
+    </div>
+</div>
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
