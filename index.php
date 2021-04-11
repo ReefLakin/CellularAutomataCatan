84b96a67becf4a5cd7cb4e2658175cc55b6800e8
ReefLakin <reeflakin2@gmail.com>
2021-04-11 10:32:46 +0100

    Hotfix: Homepage Broken
    Fixed some internal stuff that was causing issues with the homepage.

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="styles/bootstrap.css">
+    <link rel="stylesheet" href="styles/lux.css">
 
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="styles/style.css" rel="stylesheet" type="text/css">
 
     <!-- Title -->
     <title>Cellular Automata Catan</title>
